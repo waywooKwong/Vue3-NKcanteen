e6abf6e12f0f0784178058178079164e76103686
waywooKwong <2211992@mail.nankai.edu.cn>
2024-06-11 21:22:56 +0800

    fix: sql 语句调整

--- server/seat_reserve.sql
+++
@@ -1,9 +0,0 @@
--- 创建预约信息表
-create table reservation(
-	id int auto_increment primary key, --预约id
-    canteen varchar(255), -- 餐厅名称
-    floor varchar(255), --楼层
-    seatgroup varchar(255), --座位分区
-    seatnumber varchar(255), --座位号
-    time int --预约时间
-);
